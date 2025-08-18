ab630f945ac881b0d14903585540d3cec6554e8e
CrashTestAlex <alexkola109@icloud.com>
2025-08-17 20:01:22 -0400

    Delete __init__...

--- __init__.py
+++
@@ -1,9 +0,0 @@
-from typing import TYPE_CHECKING
-
-from ballsdex.packages.battle.cog import Brawl
-
-if TYPE_CHECKING:
-    from ballsdex.core.bot import BallsDexBot
-
-async def setup(bot: "BallsDexBot"):
-    await bot.add_cog(Battle(bot))
